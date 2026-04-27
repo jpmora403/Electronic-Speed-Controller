18ec0b55ef16d5cdcb7bff1107ffcaedc2b90195
Juanpablo Mora <jpmora403@gmail.com>
2026-04-26 21:29:02 -0500

    pwm input changed to pc0, confirmed working

--- src/tcb.c
+++ src/tcb.c
@@ -7,34 +7,31 @@
 
 void tcb0(void) {
 
-    PORTA.PIN7CTRL = 0;
-    PORTA.EVGENCTRLA = 7;
-    PORTA.DIRSET = 1 << 7;
+    PORTC.PIN0CTRL = 1 << 3;
+    PORTC.EVGENCTRLA = 0;
+//    PORTA.DIRSET = 1 << 7;
     //Channel selection
-    EVSYS.CHANNEL0 = EVSYS_CHANNEL_PORTA_EV1_gc;
+    EVSYS.CHANNEL0 = 0x44;
     EVSYS.USERTCB0CAPT = EVSYS_USER_CHANNEL0_gc;
 
     //Configure TCB0
-    TCB0.CTRLA = 0x1;
+    TCB0.CTRLA = 0x1; 
     TCB0.CTRLB = 0x4;
-    TCB0.EVCTRL = 0x1;
+    TCB0.EVCTRL = 0x1 | (1 << 6);
     TCB0.INTCTRL = 0x3;
     
 }
 
 ISR (TCB0_INT_vect) {
-
-    if (TCB0.INTFLAGS & 0x2) {
+   if (TCB0.INTFLAGS & 0x2) {
+        throttle = 0;
         mode = IDLE;
-        TCB0.INTFLAGS |= 0x20;
-    }else {
-        if (mode == IDLE) 
-            mode = STARTUP;
-        else {
+        TCB0.INTFLAGS = 0x2;
+    }
+    if (TCB0.INTFLAGS & 0x1) {
             mode = RUNNING;
-            uint16_t temp = TCB0.CCMP - 1000;
-            throttle = (temp * 416) / 1000;
-        }
+            throttle = TCB0.CCMP;
+        
     }
 }
 
